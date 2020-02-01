282f7b9c09f0708d82878598af3622a31c7e5a32
Miguel Ramírez <miguelramidev@gmail.com>
2020-01-31 22:53:35 -0400

    Se generó una versión de producción (ubicada en la carpeta apk de la raíz)

--- android/app/src/main/java/com/pokedex/MainActivity.java
+++ android/app/src/main/java/com/pokedex/MainActivity.java
@@ -1,6 +1,8 @@
 package com.pokedex;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
