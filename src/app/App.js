2af5364e7e2a553bb719b117fcd1510a4c53bbcf
Miguel Ramírez <miguelramidev@gmail.com>
2020-01-31 21:12:18 -0400

    Creación del Splash Creen y el icono de la aplicación, tambien se cambióel nombre de la app

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-view';
 import { MenuProvider } from 'react-native-popup-menu';
+import SplashScreen from 'react-native-splash-screen';
 
 import { AppLayout } from './components/AppLayout';
 import Routes from './Routes';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    SplashScreen.hide();
+    setLoading(false);
+  }, []);
+
   return (
     <AppLayout>
-      <StatusBar barStyle="light-content" backgroundColor="red" />
+      {
+        loading ? <StatusBar barStyle="light-content" /> :
+        <StatusBar barStyle="light-content" backgroundColor="red" />
+      }
       <SafeAreaProvider>
         <MenuProvider>
           <Routes />
